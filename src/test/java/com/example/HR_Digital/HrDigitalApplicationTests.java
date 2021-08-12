b825d7ea084e28a4841cb65e40b9189f291fe1ab
meannrp1234 <narinpron.nua@rmutto.ac.th>
2021-08-12 23:28:43 +0700

    1

---
+++ src/test/java/com/example/HR_Digital/HrDigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HR_Digital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrDigitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
